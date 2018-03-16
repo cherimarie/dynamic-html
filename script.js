4b65c7d45f78c4b189516e0c16f4daac620c59a1
cheri allen <cheri@joindrift.com>
2018-03-15 17:26:10 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var products = [
+  {name: "princess", price: 39.99, image: "https://www.glamourmutt.com/assets/images/polka-dot-designer-dog-life-jacket--hot-pink-1.jpg"},
+  {name: "pool shark", price: 46.99, image: "https://odditymall.com/includes/content/shark-fin-dog-life-jacket-0.jpg"},
+  {name: "turbo", price: 29.99, image: "http://www.petmywiener.com/images/thumbs/0002517_460.jpeg"}
+]
+
+function buildListings(){
+  // select element with id "target"
+
+  // give it a class of "parent"
+
+
+  // loop through the products array, and for each, a listing like:
+  // <div>
+  //   <h2>Product Name</h2>
+  //   <p>Product Price</p>
+  //   <img src="" alt="">
+  // </div>
+}
+
+buildListings()
+
